968aff8354e15070e632369501019ffb7a2455f8
Oskari Heinonen <oskari.heinonen@tuni.fi>
2023-12-01 13:28:20 +0200

    initial commit

---
+++ AudioPlayer.hh
@@ -0,0 +1,20 @@
+#ifndef AUDIOPLAYER_HH
+#define AUDIOPLAYER_HH
+
+#include "Include/fmod_studio.hpp"
+#include "Include/fmod.hpp"
+
+
+
+class AudioPlayer {
+
+    public:
+
+    AudioPlayer();
+
+    ~AudioPlayer();
+
+};
+
+
+#endif
