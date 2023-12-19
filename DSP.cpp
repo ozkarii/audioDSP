977fd860c87e269fd28f5bb52716a71bb802b9c7
Oskari Heinonen <oskari.heinonen@tuni.fi>
2023-12-19 11:54:47 +0200

    implemented inverse fft

--- DSP.cpp
+++ DSP.cpp
@@ -1,5 +1,5 @@
-#include "include/DSP.hh"
 #include <iostream>
+#include "include/DSP.hh"
 
 
 std::vector<float> DSP::convolution(const std::vector<float> &sound,
@@ -62,7 +62,61 @@ cVector DSP::fft(cVector &input)
     return result;
 }
 
-// add zeros until the length of input is a power of two
+cVector DSP::ifftUnnormalized(cVector &input)
+{
+    unsigned int len = input.size();
+    cVector odd;
+    cVector even;
+    cVector oddFft;
+    cVector evenFft;
+    std::complex<double> im(0, 1);
+
+    if (len == 1) 
+    {
+        return input;
+    }
+
+    for (unsigned int n(0); n < len; n += 2)
+    {
+        even.push_back(input.at(n));
+    }
+
+    for (unsigned int n(1); n < len; n += 2) 
+    {
+        odd.push_back(input.at(n));
+    }
+
+    evenFft = ifftUnnormalized(even);
+    oddFft = ifftUnnormalized(odd);
+
+    int halfLen = len / 2;
+
+    cVector result(len);
+
+    for (int n = 0; n < halfLen; ++n)
+    {
+        std::complex<double> t = std::polar(1.0, 2 * M_PI * n / len) * oddFft.at(n);
+        result.at(n) = (evenFft.at(n) + t);
+        result.at(n + halfLen) = (evenFft.at(n) - t);
+    }
+
+    return result;
+}
+
+std::vector<double> DSP::ifft(cVector &input)
+{
+    cVector unnormalized = DSP::ifftUnnormalized(input);
+    std::vector<double> output(unnormalized.size());
+
+    for (unsigned int i = 0; i < unnormalized.size(); i++)
+    {
+        output.at(i) = unnormalized.at(i).real()/unnormalized.size();
+    }
+
+    return output;
+}
+
+// add zeros to the end until the length of input is a power of two
 void DSP::zeroPad(cVector &input)
 {
     unsigned long int nearestPow2 = 1;
--- include/DSP.hh
+++ include/DSP.hh
@@ -9,10 +9,15 @@ using cVector = std::vector<std::complex<double>>;
 
 namespace DSP
 {
+
     std::vector<float> convolution(const std::vector<float> &sound,
                                    const std::vector<float> &ir);
     
     cVector fft(cVector &input);
+
+    cVector ifftUnnormalized(cVector &input);
+
+    std::vector<double> ifft(cVector &input);
 
     void zeroPad(cVector &input);
 
