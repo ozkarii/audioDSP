a6179d9af67abce6e34afd96941d5a745a389879
Oskari Heinonen <oskari.heinonen@tuni.fi>
2023-12-22 21:33:00 +0200

    implement filter command and test for conv

--- Main.cpp
+++ Main.cpp
@@ -5,10 +5,6 @@
 #include "include/DSP.hh"
 #include "include/Player.hh"
 
-// adt play haha.wav
-// adt filter haha.wav st_andrew.wav -o out.wav -p
-// 
-
 #define GET_VARIABLE_NAME(Variable) (#Variable)
 
 std::string toLowerCase(std::string &s)
@@ -64,6 +60,7 @@ void printVector(std::vector<double> &input, std::string header = "",
     }
 }
 
+// void filter(std::string &a, std::string &b);
 
 int main(int argc, char* argv[]) 
 {
@@ -91,6 +88,11 @@ int main(int argc, char* argv[])
             std::cout << "no file provided" << std::endl;
             return 1;
         }
+        if (argc != 3)
+        {
+            std::cout << "Wrong number of params" << std::endl;
+            return 1;
+        }
         if ( not(audioPlayer.loadWav(args.at(2).c_str())) ) 
         {
             std::cout << "error loading file" << std::endl;
@@ -102,31 +104,102 @@ int main(int argc, char* argv[])
 
     if (toLowerCase(args.at(1)) == "test")
     {
-        cVector a = {2, 3, 4, 1, 3, 5, 29, 1};
-        cVector b = {6, -2, 3, 9};
+        if (args.at(2) == "--fft") 
+        {
+            cVector a = {2, 3, 4, 1, 3, 5, 29, 1};
+            cVector b = {6, -2, 3, 9};
 
-        printVector(a, "a:");
-        printVector(b, "b:");
+            std::vector<double> a_real = {2, 3, 4, 1, 3, 5, 29, 1};
+            std::vector<double> b_real = {6, -2, 3, 9};
 
-        DSP::zeroPad(a);
-        DSP::zeroPad(b);
+            printVector(a, "a:");
+            printVector(b, "b:");
 
-        printVector(a, "a_0:");
-        printVector(b, "b_0:");
+            DSP::zeroPad(a);
+            DSP::zeroPad(b);
 
-        cVector a_fft = DSP::fft(a);
-        cVector b_fft = DSP::fft(b);
+            printVector(a, "a_0:");
+            printVector(b, "b_0:");
 
-        printVector(a_fft, "fft_a:");
-        printVector(b_fft, "fft_b:");
+            cVector a_fft = DSP::fft(a);
+            cVector b_fft = DSP::fft(b);
 
-        std::vector<double> a_ifft = DSP::ifft(a_fft);
-        std::vector<double> b_ifft = DSP::ifft(b_fft);
+            printVector(a_fft, "fft_a:");
+            printVector(b_fft, "fft_b:");
 
+            std::vector<double> a_ifft = DSP::ifft(a_fft);
+            std::vector<double> b_ifft = DSP::ifft(b_fft);
 
-        printVector(a_ifft, "ifft_a:");
-        printVector(b_ifft, "ifft_b:");
+            printVector(a_ifft, "ifft_a:");
+            printVector(b_ifft, "ifft_b:");
+            
+            std::vector<double> convAB = DSP::convolution(a_real, b_real);
+            printVector(convAB, "conv:");
 
+            return 0;
+        }
+
+        if (args.at(2) == "--conv") 
+        {
+            cVector a = {2, 3, 4, 1, 3, 5, 29, 1};
+            cVector b = {6, -2, 3, 9};
+
+            std::vector<double> a_real = {2, 3, 4, 1, 3, 5, 29, 1};
+            std::vector<double> b_real = {6, -2, 3, 9};
+                        
+            std::vector<double> convAB = DSP::convolution(a_real, b_real);
+            
+            printVector(convAB, "conv:");
+
+            return 0;
+        }
+    }
+
+    if (toLowerCase(args.at(1)) == "filter")
+    {
+        if (argc < 4)
+        {
+            std::cout << "Wrong number of params" << std::endl;
+            return 1;
+        }
+
+        AudioFile<double> aFile = AudioUtils::createAudioFile(args.at(2).c_str());
+        AudioFile<double> bFile = AudioUtils::createAudioFile(args.at(3).c_str());
+
+        AudioFile<double>* spec = &aFile;
+
+        std::vector<double> aSamplesL = aFile.samples.at(0);
+        std::vector<double> bSamplesL = bFile.samples.at(0);
+
+        std::vector<double> leftConv = DSP::convolution(aSamplesL, bSamplesL);
+
+        AudioFile<double> convFile = AudioUtils::samplesToAudioFile(
+                          leftConv, leftConv, AudioUtils::createWavInfo(spec));
+        
+        AudioUtils::writeWavFile(convFile, "out.wav");
+
+        if (args.at(4) == "-p")
+        {
+            if ( not(audioPlayer.loadWav("out.wav")) ) 
+            {
+                std::cout << "error loading file" << std::endl;
+                return 1;
+            }
+            audioPlayer.play();
+        }
+        
+        return 0;
+
+    }
+
+    if (args.at(1) == "info")
+    {
+        if (argc != 3)
+        {
+            return 1;
+        }
+
+        AudioUtils::printWavInfo(args.at(2));
         return 0;
     }
 
