48ff04a9ce7f0cd65154248f8a783885335894e0
Oskari Heinonen <oskari.heinonen@tuni.fi>
2024-02-02 13:53:07 +0200

    add comments

--- include/Math.hh
+++ include/Math.hh
@@ -6,15 +6,36 @@ using cVector = std::vector<std::complex<double>>;
 
 namespace Math {
 
+    /// @brief Takes two equal length vectors and multiplies them element wise.
+    /// @param a vector 1
+    /// @param b vector 2
+    /// @return element wise product vector
     std::vector<double> multiplyElementWise(std::vector<double> &a,
                                             std::vector<double> &b);
     
+    /// @brief Takes two equal length vectors and multiplies them element wise.
+    /// @param a vector 1
+    /// @param b vector 2
+    /// @return element wise product vector
     cVector multiplyElementWise(cVector &a, cVector &b);
 
+
+    /// @brief sin(x)/x
+    /// @param input
+    /// @return sinc of input
     double sinc(double input);
 
+    /// @brief sin(x)/x
+    /// @param input
+    /// @param x0 output when input equals zero
+    /// @return sinc of input
     double sinc(double input, double x0);
 
+    /// @brief sin(x)/x
+    /// @param input
+    /// @param x0 output when input equals zero
+    /// @param normalize wheter or not to normalize by multiplying input by pi
+    /// @return sinc of input
     double sinc(double input, double x0, bool normalize);
-
+    
 };
